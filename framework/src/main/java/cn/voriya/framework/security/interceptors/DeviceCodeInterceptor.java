c1bb94b9b3fd3fa3b0abc64668df7cf2c4dea6b9
JasirVoriya <jasirvoriya@gmail.com>
2023-12-01 19:47:44 +0800

    新增设备码检查拦截器，如果请求里面没有设备码，将直接抛出异常

---
+++ framework/src/main/java/cn/voriya/framework/security/interceptors/DeviceCodeInterceptor.java
@@ -0,0 +1,21 @@
+package cn.voriya.framework.security.interceptors;
+
+import cn.voriya.framework.security.context.UserContext;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+@Slf4j
+public class DeviceCodeInterceptor {
+    /**
+     * 拦截所有Controller和RestController
+     */
+    @Before("@within(org.springframework.stereotype.Controller)||@within(org.springframework.web.bind.annotation.RestController)")
+    public void interceptor() {
+        //检查是否有设备码，如果没有则抛出异常
+        UserContext.getCurrentUserUUID();
+    }
+}
