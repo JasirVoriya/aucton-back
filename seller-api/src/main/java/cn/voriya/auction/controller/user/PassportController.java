a73090bf4a3c3578e4330c81f59e6727d73896d6
JasirVoriya <jasirvoriya@gmail.com>
2023-12-02 19:56:03 +0800

    修复登录、注册接口返回值未使用统一返回类的bug

--- seller-api/src/main/java/cn/voriya/auction/controller/user/PassportController.java
+++ seller-api/src/main/java/cn/voriya/auction/controller/user/PassportController.java
@@ -1,7 +1,9 @@
 package cn.voriya.auction.controller.user;
 
 import cn.voriya.auction.service.IUserService;
+import cn.voriya.framework.entity.vo.ResultMessage;
 import cn.voriya.framework.security.token.Token;
+import cn.voriya.framework.utils.ResultUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +26,9 @@ public class PassportController {
      * @return token
      */
     @PostMapping("login/password")
-    public Token passwordLogin(String account, String password) {
-        return userService.passwordLogin(account,password);
+    public ResultMessage<Token> passwordLogin(String account, String password) {
+        final Token token = userService.passwordLogin(account, password);
+        return ResultUtil.data(token);
     }
 
     /**
@@ -36,7 +39,8 @@ public class PassportController {
      * @return token
      */
     @PostMapping("login/code")
-    public Token codeLogin(String email, String code) {
-        return userService.codeLogin(email,code);
+    public ResultMessage<Token> codeLogin(String email, String code) {
+        final Token token = userService.codeLogin(email, code);
+        return ResultUtil.data(token);
     }
 }
