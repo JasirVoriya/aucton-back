67f68859185bba3048fb52c33c899bb7e153c92e
JasirVoriya <jasirvoriya@gmail.com>
2023-12-03 21:02:56 +0800

    更新商品字段

--- seller-api/src/main/java/cn/voriya/auction/entity/dos/Goods.java
+++ seller-api/src/main/java/cn/voriya/auction/entity/dos/Goods.java
@@ -1,10 +1,16 @@
 package cn.voriya.auction.entity.dos;
 
 import cn.voriya.framework.mybatis.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * <p>
@@ -19,15 +25,17 @@ import lombok.EqualsAndHashCode;
 public class Goods extends BaseEntity {
 
     /**
-     * 店家ID
+     * 送拍机构ID
      */
     @JsonSerialize(using = ToStringSerializer.class)
     private Long sellerId;
+
     /**
      * 分类ID
      */
     @JsonSerialize(using = ToStringSerializer.class)
     private Long categoryId;
+
     /**
      * 商品名称
      */
@@ -47,4 +55,39 @@ public class Goods extends BaseEntity {
      * 封面路径
      */
     private String cover;
+
+    /**
+     * 起拍价
+     */
+    private BigDecimal startingPrice;
+
+    /**
+     * 最低加价
+     */
+    private BigDecimal increment;
+
+    /**
+     * 保证金
+     */
+    private BigDecimal deposit;
+
+    /**
+     * 开始时间
+     */
+    private LocalDateTime startTime;
+
+    /**
+     * 结束时间
+     */
+    private LocalDateTime endTime;
+
+    private Boolean deleteFlag;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private String createBy;
+
+    private String updateBy;
 }
