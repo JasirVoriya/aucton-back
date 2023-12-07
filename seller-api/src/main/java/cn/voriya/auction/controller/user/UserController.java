da028d3639a39698e0a46ba39b6120f0986864af
JasirVoriya <jasirvoriya@gmail.com>
2023-12-07 22:05:21 +0800

    分页查询用户信息

--- seller-api/src/main/java/cn/voriya/auction/controller/user/UserController.java
+++ seller-api/src/main/java/cn/voriya/auction/controller/user/UserController.java
@@ -1,18 +1,38 @@
 package cn.voriya.auction.controller.user;
 
+import cn.voriya.auction.entity.dos.User;
+import cn.voriya.auction.service.IUserService;
+import cn.voriya.framework.entity.vo.ResultMessage;
+import cn.voriya.framework.utils.ResultUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author JasirVoriya
  * @since 2023-12-02
  */
-@Controller
+@RestController
 @RequestMapping("user")
 public class UserController {
+    private final IUserService userService;
 
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * 分页获取所有用户
+     */
+    @GetMapping("all")
+    public ResultMessage<Page<User>> getAllUser(Integer page, Integer size) {
+        final Page<User> userPage = userService.getBaseMapper().selectPage(new Page<>(page, size), null);
+        return ResultUtil.data(userPage);
+
+    }
 }
